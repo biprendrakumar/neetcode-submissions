2e5ab1a75aa6c2ff9e772b68f1a22d34c868f2cb
biprendra kumar <singhviprendra@gmail.com>
2026-09-03 10:20:02 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        return sol(s,t,0,0);
+    }
+    public static boolean sol(String s, String t,int i,int j){
+        if(i==s.length()){
+            return true;
+        }
+        if(j==t.length()){
+            return false;
+        }
+        if(s.charAt(i)==t.charAt(j)){
+            return sol(s,t,i+1,j+1);
+        }
+        else{
+            return sol(s,t,i,j+1);
+        }
+    }
+}
